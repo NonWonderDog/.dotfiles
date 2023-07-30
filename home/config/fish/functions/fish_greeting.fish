92a821b1943225aff9e239d260648c5be59cffc8
NonWonderDog <nonwonderdog@gmail.com>
2023-07-30 12:06:38 -0400

    silliness

--- home/config/fish/functions/fish_greeting.fish
+++ home/config/fish/functions/fish_greeting.fish
@@ -1,5 +1,28 @@
 function fish_greeting
-    if test -e /usr/local/games/fortune
-        /usr/local/games/fortune
+    command -q fortune; and set -l quote "fortune"; or set -l quote ""
+    set -l art ""
+
+    if command -q cowfortune
+        set art "$(cowfortune)"
+    else if command -q fortune; and command -q cowsay
+        set -l cow (find /usr/share/cows -type f -name "*.cow" | shuf -n1)
+        set -l cowsay (shuf -n1 -e "cowsay" "cowthink")
+        set art "\${c1}$(fortune | $cowsay -f $cow -W 60)"
+    end
+
+    if command -q neofetch
+        if test -n "$art"
+            neofetch --disable Packages --ascii "$art"
+            set quote ""
+        else
+            neofetch --disable Packages
+        end
+    else
+        if test -n "$art"
+            echo $art
+        end
+    end
+    if test -n "$quote"
+        $quote
     end
 end
