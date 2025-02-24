07f24b8c50953282457f008c96d992b8a77c5be8
NonWonderDog <nonwonderdog@gmail.com>
2025-02-24 08:47:13 -0500

    Fix treesitter config

--- home/config/nvim/lua/plugins/treesitter.lua
+++ home/config/nvim/lua/plugins/treesitter.lua
@@ -2,18 +2,27 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = {
-                "vim", "vimdoc", "lua", "query",
-                "bash", "fish", "markdown_inline",
-                "rust",
-            },
-            ignore_install = {},
-            sync_install = false,
-            auto_install = true,
-            modules = {},
-            highlight = { enable = true },
-            indent = { enable = true },
-        }
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = {
+                    "c", "vim", "vimdoc", "lua", "query",
+                    "bash", "fish", "markdown_inline",
+                    "rust", "python"
+                },
+                ignore_install = {},
+                sync_install = false,
+                auto_install = true,
+                modules = {},
+                highlight = { enable = true },
+                indent = { enable = true },
+                disable = function(_, buf)
+                    local max_filesize = 1024 * 1024 -- 1 MB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+            })
+        end,
     }
 }
