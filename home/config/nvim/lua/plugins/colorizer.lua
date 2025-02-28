a8c71c17e011135c00077427a1986a1f954cddca
NonWonderDog <nonwonderdog@gmail.com>
2025-02-28 15:50:03 -0500

    Use end-of-line color annotations

--- home/config/nvim/lua/plugins/colorizer.lua
+++ home/config/nvim/lua/plugins/colorizer.lua
@@ -3,10 +3,12 @@ return {
         'brenoprata10/nvim-highlight-colors',
         opts = {
             render = "virtual",
-            virtual_symbol = '█▉',
+            -- virtual_symbol_position = 'inline',
+            -- virtual_symbol = '█▉',
+            virtual_symbol_position = 'eol',
+            virtual_symbol = '██',
             virtual_symbol_prefix = '',
             virtual_symbol_suffix = '',
-            virtual_symbol_position = 'inline',
         }
     }
 }
